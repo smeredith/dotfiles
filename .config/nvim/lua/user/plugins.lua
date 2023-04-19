fa2ed1a5f182047d75427d3837b0d083e681ed57
Steve Meredith <smeredith@users.noreply.github.com>
2023-04-19 12:12:09 -0700

    Add gitsigns

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -44,6 +44,7 @@ return packer.startup(function(use)
     use { "nvim-telescope/telescope.nvim", tag = "0.1.1", requires = { {"nvim-lua/plenary.nvim"} } }
     use { "nvim-telescope/telescope-file-browser.nvim", requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" } }
     use { "folke/which-key.nvim" }
+    use { "lewis6991/gitsigns.nvim" }
 
     -- Colorschemes
     use "jeffkreeftmeijer/vim-dim"
