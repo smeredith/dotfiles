b28193511459d90aa2c58f7e1a0e8b07b7dd68b5
Steve Meredith <smeredith@users.noreply.github.com>
2023-04-19 12:02:45 -0700

    Add basic nvim config

---
+++ .config/nvim/lua/user/telescope.lua
@@ -0,0 +1,37 @@
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+  return
+end
+
+local actions = require "telescope.actions"
+
+telescope.setup {
+  defaults = {
+
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
+  pickers = {
+    buffers = {
+      ignore_current_buffer = true,
+      sort_lastused = true,
+    },
+  },
+  extensions = {
+    file_browser = {
+      depth = 1,
+      sorting_strategy = 'ascending',
+      layout_config = {
+        prompt_position = 'top',
+      }
+    },
+  },
+}
+
+-- Enable telescope fzf native, if installed
+pcall(require('telescope').load_extension, 'fzf')
+pcall(require('telescope').load_extension, 'file_browser')
