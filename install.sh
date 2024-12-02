06cf174ae3518d49d59ad5852b2e9361cc9a022d
Steve Meredith <meredith.steve@gmail.com>
2024-12-01 17:59:33 -0800

    Make install.sh safer

--- install.sh
+++ install.sh
@@ -1,18 +1,12 @@
-!/bin/bash
-
 create_symlinks() {
     # Get the directory in which this script lives.
     script_dir=$(dirname "$(readlink -f "$0")")
 
-    # Get a list of all files in this directory that start with a dot.
-    files=$(find -maxdepth 1 -name ".*")
+    files=".bash_aliases .config/tmux .config/nvim"
 
     # Create a symbolic link to each dot file and dir in the home directory.
     for file in $files; do
-        name=$(basename $file)
-        echo "Creating symlink to $name in home directory."
-        rm -rf ~/$name
-        ln -s $script_dir/$name ~/$name
+        ln -s $script_dir/$file ~/$file
     done
 }
 
