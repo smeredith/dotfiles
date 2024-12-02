4854afb30cae619088642f1ddecec5aa96e54733
Steve Meredith <meredith.steve@gmail.com>
2024-12-02 05:34:50 -0800

    Update whichkey mappings

--- .config/nvim/lua/user/whichkey.lua
+++ .config/nvim/lua/user/whichkey.lua
@@ -8,9 +8,18 @@ local mappings = {
   { "<leader>c", "<cmd>bdelete!<cr>", desc="Close Buffer" },
   { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc="Explorer" },
   { "<leader>h", "<cmd>nohlsearch<cr>", desc="No Highlight" },
-  { "<leader>w", "<cmd>set wrap!<cr>", desc="Toggle Wrap" },
   { "<leader>s", group = "search" },
   { "<leader>sb", "<cmd>Telescope buffers<cr>", desc="Buffers" },
+  { "<leader>sc", "<cmd>Telescope commands<cr>", desc="Commands" },
+  { "<leader>sf", "<cmd>Telescope find_files<cr>", desc="Find Files" },
+  { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc="Live Grep" },
+  { "<leader>sG", function() require('telescope.builtin').grep_string({search = vim.fn.input('Rg> ')}) end, desc="Live Grep" },
+  { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc="Keymaps" },
+  { "<leader>sl", "<cmd>Telescope resume<cr>", desc="Resume Last Search" },
+  { "<leader>sr", "<cmd>Telescope oldfiles<cr>", desc="Open Recent File" },
+  { "<leader>sR", "<cmd>Telescope registers<cr>", desc="Registers" },
+  { "<leader>w", "<cmd>set wrap!<cr>", desc="Toggle Wrap" },
+  { "<leader>z", "<cmd>set spell!<cr>", desc="Toggle Spellcheck" },
 }
 
 which_key.setup(options)
