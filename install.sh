1dfd8c3091cf6f36422850211bc643b381f9180a
Steve Meredith <smeredith@users.noreply.github.com>
2025-06-08 11:36:12 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ create_symlinks() {
     # Get the directory in which this script lives.
     script_dir=$(dirname "$(readlink -f "$0")")
 
-    files=".config/tmux .config/nvim"
+    files=".bash_aliases .config/tmux .config/nvim"
 
     # Create a symbolic link to each dot file and dir in the home directory.
     for file in $files; do
