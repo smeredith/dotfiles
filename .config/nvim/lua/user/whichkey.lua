3cda6ea942fdf8b373ec3507c65a92a740adaa54
Steve Meredith <meredith.steve@gmail.com>
2025-06-10 12:54:26 -0700

    Add shortcut to toggle git signs

--- .config/nvim/lua/user/whichkey.lua
+++ .config/nvim/lua/user/whichkey.lua
@@ -6,8 +6,8 @@ local options = {
 
 local mappings = {
   { "<leader>c", "<cmd>bdelete!<cr>", desc="Close Buffer" },
-  { "<leader>h", "<cmd>nohlsearch<cr>", desc="No Highlight" },
-  { "<leader>s", group = "search" },
+  { "<leader>t", group = "Toggle" },
+  { "<leader>s", group = "Search" },
   { "<leader>sb", "<cmd>Telescope buffers<cr>", desc="Buffers" },
   { "<leader>sc", "<cmd>Telescope commands<cr>", desc="Commands" },
   { "<leader>sf", "<cmd>Telescope find_files<cr>", desc="Find Files" },
@@ -17,9 +17,11 @@ local mappings = {
   { "<leader>sl", "<cmd>Telescope resume<cr>", desc="Resume Last Search" },
   { "<leader>sr", "<cmd>Telescope oldfiles<cr>", desc="Open Recent File" },
   { "<leader>sR", "<cmd>Telescope registers<cr>", desc="Registers" },
-  { "<leader>n", "<cmd>set number!<cr>", desc="Toggle Line Number" },
-  { "<leader>w", "<cmd>set wrap!<cr>", desc="Toggle Wrap" },
-  { "<leader>z", "<cmd>set spell!<cr>", desc="Toggle Spellcheck" },
+  { "<leader>tg", function() require('gitsigns').toggle_signs() end, desc="Git Signs" },
+  { "<leader>th", "<cmd>set hlsearch!<cr>", desc="Highligh Search" },
+  { "<leader>tn", "<cmd>set number!<cr>", desc="Line Number" },
+  { "<leader>tw", "<cmd>set wrap!<cr>", desc="Wrap" },
+  { "<leader>tz", "<cmd>set spell!<cr>", desc="Spellcheck" },
 }
 
 which_key.setup(options)
