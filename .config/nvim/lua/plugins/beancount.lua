bddddcdedc6a522f651218a1c99653f3d54b6689
Steve Meredith <meredith.steve@gmail.com>
2026-04-22 18:52:03 -0700

    Update beancount plugin

--- .config/nvim/lua/plugins/beancount.lua
+++ .config/nvim/lua/plugins/beancount.lua
@@ -4,17 +4,18 @@ return {
     dependencies = {
         {
             "saghen/blink.cmp",
-            optional = true,
+            version = "1.*",
+            dependencies = { "saghen/blink.lib", version = "1.*" },
+            build = function() require("blink.cmp").build():wait(60000) end,
             opts = function(_, opts)
+                opts.sources = opts.sources or {}
+                opts.sources.default = opts.sources.default or { "lsp", "path", "snippets", "buffer" }
                 table.insert(opts.sources.default, "beancount")
                 opts.sources.providers = opts.sources.providers or {}
                 opts.sources.providers.beancount = {
                     name = "beancount",
                     module = "beancount.completion.blink",
                     score_offset = 100,
-                    opts = {
-                        trigger_characters = { ":", "#", "^", '"', " " },
-                    },
                 }
                 return opts
             end,
@@ -26,6 +27,9 @@ return {
     config = function()
         require("beancount").setup({
             python_path = vim.fn.expand("~/.local/share/pipx/venvs/beancount/bin/python"),
+            separator_column = 60,
+            auto_fill_amounts = true,
+            instant_alignment = false,
         })
         vim.api.nvim_create_autocmd("FileType", {
             pattern = { "beancount", "bean" },
