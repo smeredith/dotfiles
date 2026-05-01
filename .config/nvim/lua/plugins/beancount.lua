4f18b10332318d484172a5bb2cdc0787d4f80006
Steve Meredith <meredith.steve@gmail.com>
2026-04-30 21:27:27 -0700

    Make beancount snippets work.

--- .config/nvim/lua/plugins/beancount.lua
+++ .config/nvim/lua/plugins/beancount.lua
@@ -1,24 +1,7 @@
 return {
     "hxueh/beancount.nvim",
-    ft = { "beancount", "bean" },
+    ft = { "beancount" },
     dependencies = {
-        {
-            "saghen/blink.cmp",
-            version = "1.*",
-            build = function() require("blink.cmp").build():wait(60000) end,
-            opts = function(_, opts)
-                opts.sources = opts.sources or {}
-                opts.sources.default = opts.sources.default or { "lsp", "path", "snippets", "buffer" }
-                table.insert(opts.sources.default, "beancount")
-                opts.sources.providers = opts.sources.providers or {}
-                opts.sources.providers.beancount = {
-                    name = "beancount",
-                    module = "beancount.completion.blink",
-                    score_offset = 100,
-                }
-                return opts
-            end,
-        },
         {
             "L3MON4D3/LuaSnip",
         },
@@ -28,7 +11,7 @@ return {
             python_path = vim.fn.expand("~/.local/share/pipx/venvs/beancount/bin/python"),
             separator_column = 75,
             instant_alignment = true,
-            auto_format_on_save = true,
+            auto_format_on_save = false,
             auto_fill_amounts = true,
             inlay_hints = true,
             auto_save_before_check = false,
@@ -41,8 +24,6 @@ return {
             pattern = { "beancount" },
             callback = function(args)
                 vim.treesitter.start(args.buf)
-                vim.opt_local.foldenable = false
-                vim.opt_local.foldmethod = "manual"
                 vim.opt.tabstop = 2
                 vim.opt.softtabstop = 2
                 vim.opt.shiftwidth = 2
